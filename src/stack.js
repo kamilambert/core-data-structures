18aa3fc3a0ad4508570398a104c096dcbc51dd93
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-04-12 18:12:37 -0700

    All stack tests passing

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,41 @@
-'use strict'
-
-export default class Stack {
-  // your code here
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
 }
+
+class Stack {
+  constructor() {
+    this.top = 0
+    this.dataStore = []
+  }
+
+  push(newItem) {
+    this.dataStore[this.top] = newItem
+    this.top++
+  }
+
+  pop() {
+    if(this.top === 0) {
+      return null
+    } else {
+      this.top--
+      return this.dataStore[this.top]
+    }
+  }
+
+  isEmpty() {
+    if (this.top === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  length() {
+    return this.top
+  }
+}
+
+export { Node, Stack }
